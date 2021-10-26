3996e33fa74b796a5f8d6e96cff2d8282995f2e7
Vinicius Lins <viniciusaclins@gmail.com>
2021-10-26 19:36:27 -0300

    Primeiro commit da aplicaçao

---
+++ app.js
@@ -0,0 +1,31 @@
+const peoples = [
+    {
+        "name": "Vinícius",
+        "departament": "Presidente",
+        "age": 20
+    },
+    {
+        "name": "Negão",
+        "departament": "Diretor de Qualidade",
+        "age": 22
+    },
+    {
+        "name": "Fernando",
+        "departament": "Diretor de Projetos",
+        "age": 24
+    },
+    {
+        "name": "Magão",
+        "departament": "Diretor de Negócios",
+        "age": 20
+    },
+    {
+        "name": "Pamplona",
+        "departament": "Diretor de Gente & Gestão",
+        "age": 20
+    },
+]
+
+peoples.forEach(people => {
+    if (people.age < 22) console.log(people.name);
+});
